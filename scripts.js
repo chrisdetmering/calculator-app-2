067203c273bb75ac2eb8ea32a7ac2a61e59f9515
froggy <chrisdetmering@gmail.com>
2021-03-16 13:04:09 -0700

    Andy review completed

--- scripts.js
+++ scripts.js
@@ -1,96 +1,83 @@
-const input = document.querySelector(".cal-display");
-let mathSymbol = "";
+const display = document.querySelector(".cal-display");
+let operation = null;
 let firstNumber = null;
 let secondNumber = null;
 
-
 document.querySelectorAll('.number').forEach(numberButton => { 
     numberButton.addEventListener("click", () => { 
-        const number = parseFloat(numberButton.textContent); 
-        
+        if (display.textContent.length >= 10) return; 
+
+        const number = numberButton.textContent; 
+            
         if (firstNumber === null) { 
             setFirstNumber(number); 
+            display.textContent = firstNumber; 
             return;
         }
 
+        if (firstNumber !== null && operation === null) { 
+            newNumber = firstNumber;
+            newNumber += number; 
+            setFirstNumber(newNumber);
+            display.textContent = firstNumber; 
+            return; 
+        }
+
+
         if (secondNumber === null) { 
             setSecondNumber(number); 
+            display.textContent = secondNumber; 
+            return; 
         }
+
+        if (firstNumber !== null && operation !== null && secondNumber !== null) { 
+            newNumber = secondNumber;
+            newNumber += number; 
+            setSecondNumber(newNumber);
+            display.textContent = secondNumber; 
+            return;  
+        }
+
 
     });
 })
 
+function setFirstNumber(number) { 
+    firstNumber = number; 
+}
+
+function setSecondNumber(number) { 
+    secondNumber = number; 
+}
 
 
-
-document.querySelector(".input-buttons")
-.addEventListener("click", (e)=>{
-    let clickedBox = e.target;
-    if (firstNumber === null){
-        setFirstNumber(clickedBox); 
-        return;  
-    }
-
-    if (clickedBox.className === "symbol") { 
-        mathSymbol = clickedBox.textContent;
-        return; 
-    }
-
-    if (secondNumber === null) {
-        setSecondNumber(clickedBox);
-        return; 
-    }
-
-    if (secondNumber !== null) {
-        const buttonType = clickedBox.className; 
-        if (buttonType === "number") { 
-            input.textContent += clickedBox.textContent;
-            secondNumber = parseFloat(input.textContent);
-            return; 
+document.querySelectorAll('.operator').forEach(operator => { 
+    operator.addEventListener("click", () => { 
+        if (firstNumber !== null) { 
+            operation = operator.textContent;
         }
-
-        if (buttonType === "symbol") { 
-            const answer = calculate(); 
-            const firstNumber = answer; 
-            input.textContent = firstNumber; 
-            secondNumber = null;
-            mathSymbol = buttonType.textContent; 
-        } 
-        
-    }
-}); 
-
+    })
+})
 
 document.querySelector(".equals")
 .addEventListener("click", () => {
-    if (secondNumber !== null && mathSymbol !== "" ){  
+    if (firstNumber !== null && secondNumber !== null && operation !== null ){  
         const answer = calculate(); 
-        input.textContent = answer; 
+        display.textContent = answer; 
         firstNumber = answer; 
         secondNumber = null; 
-        mathSymbol = ''; 
+        operation = null; 
     }
 });
 
 
-function setFirstNumber(number) { 
-    firstNumber = number; 
-    input.textContent += number;
-}
-
-function setSecondNumber(button) { 
-    
-}
-
-
-
 function calculate() { 
-    switch (mathSymbol){
+    switch (operation){
         case "+":
             return add(firstNumber, secondNumber);     
         case "-":
             return subtract(firstNumber, secondNumber); 
-        case "X":
+        case "x":
             return multiply(firstNumber, secondNumber);
         case "÷":
             return divide(firstNumber, secondNumber); 
@@ -98,25 +85,25 @@ function calculate() {
 }
 
 function add(num1, num2) { 
-    return num1 + num2; 
+    return `${Number(num1) + Number(num2)}`;
 }
 
 function subtract(num1, num2) { 
-    return num1 - num2; 
+    return `${Number(num1) - Number(num2)}`; 
 }
 
 function multiply(num1, num2) { 
-    return num1 * num2; 
+    return `${Number(num1) * Number(num2)}`; 
 }
 
 function divide(num1, num2) { 
-    return num1 / num2; 
+    return `${Number(num1) / Number(num2)}`; 
 }
 
 document.querySelector(".clear")
 .addEventListener('click', ()=>{
     firstNumber = null;
     secondNumber = null;
-    mathSymbol = "";
-    input.textContent = "";
+    operation = null;
+    display.textContent = "";
 });
